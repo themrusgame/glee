d8d0d2349d545bd5bb4cbdc66c0a17cd61601acb
themrusgame <themrus-game@yandex.ru>
2021-03-20 01:26:27 +0600

    added products and info

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,24 @@
 $(function(){
 
+  
+
   $('.top-slider__inner').slick({
     arrows: false,
     dots: true,
     fade: true
   });
+
+  // var mixer = mixitup('.product__items');
+
+  var containerEl = document.querySelector('.products__items');
+  var mixer;
+
+  if (containerEl) {
+    mixer = mixitup(containerEl, {
+      selectors: {
+        control: '[data-filter]'
+      }
+    });
+  }
 
 });
