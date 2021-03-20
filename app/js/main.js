14c0ab25f1b8c19f82b5b5f3a1749d1b4e36b182
themrusgame <themrus-game@yandex.ru>
2021-03-21 01:28:24 +0600

    added new design block

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
-$(function(){
+$(function () {
 
-  
+
 
   $('.top-slider__inner').slick({
     arrows: false,
@@ -8,17 +8,40 @@ $(function(){
     fade: true
   });
 
-  // var mixer = mixitup('.product__items');
 
-  var containerEl = document.querySelector('.products__items');
-  var mixer;
+  var mixer = mixitup('.products__items');
+  var mixer = mixitup('.new__items');
 
-  if (containerEl) {
-    mixer = mixitup(containerEl, {
-      selectors: {
-        control: '[data-filter]'
-      }
-    });
-  }
+  // var containerEl = document.querySelector('.products__items');
+  // var mixer;
+
+  // if (containerEl) {
+  //   mixer = mixitup(containerEl, {
+  //     selectors: {
+  //       control: '[data-filter]'
+  //     }
+  //   });
+  // }
+
+  // var containerEl1 = document.querySelector('.products__items');
+  // var containerEl2 = document.querySelector('.new__items');
+  // var mixer;
+  // var mixer1;
+
+  // if (containerEl1) {
+  //   mixer = mixitup(containerEl1, {
+  //     selectors: {
+  //       control: '[data-filter]'
+  //     }
+  //   });
+  // };
+
+  // if (containerEl2) {
+  //   mixer1 = mixitup(containerEl2, {
+  //     selectors: {
+  //       control: '[data-filter]'
+  //     }
+  //   });
+  // };
 
 });
