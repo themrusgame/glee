4cc2113d76beb35bab71eec65c9861ad74be271c
themrusgame <themrus-game@yandex.ru>
2021-03-30 20:32:19 +0600

    fixed mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -70,7 +70,19 @@ $(function () {
   });
 
 
-  var mixer = mixitup('.products__items');
-  var mixer = mixitup('.new__items');
+  // var mixer = mixitup('.products__items');
+  // var mixer = mixitup('.new__items');
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 
 });
